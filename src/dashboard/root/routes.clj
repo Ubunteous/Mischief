fd0c621159670f8ef01ae831d3a1cfe2d6cf42d6
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-12-18 22:20:33 +0100

    Started decoupling SQL requests from HTML list

--- src/dashboard/root/routes.clj
+++ src/dashboard/root/routes.clj
@@ -4,25 +4,33 @@
    [dashboard.system :as-alias system] ;; avoid cycle deps
    [hiccup2.core :as hiccup]
    [honey.sql :as sql]
-   [next.jdbc :as jdbc]
-   [next.jdbc.result-set :as rs]))
+   [next.jdbc :as jdbc]))
 
-(def sqlmap {:select [:name]
-             :from   [:chars]})
+(def sqlmap {:select [:*]
+             :from   [:story.chars]})
+
+(defn get-characters
+  [db query]
+  (jdbc/execute!
+   db
+   (sql/format sqlmap)))
 
 (defn root-handler
   [{::system/keys [db]} _request]
-  (let [{character :name}
-        (jdbc/execute-one!
-         db
-         ;; ["select name from chars"]
-         (sql/format sqlmap)
-         {:builder-fn rs/as-unqualified-lower-maps})]
+  ;; (let [{character :name} (get-characters db sqlmap)]
+  (let [characters (get-characters db (sql/format sqlmap))]
+
     {:status 200
      :headers {"Content-Type" "text/html"}
-     :body (str
-            (hiccup/html
-             (page-html/view :body [:h1 (str "Hello, " character)])))}))
+
+     :body
+     (str
+      (hiccup/html
+       [:h1 (str "Hello, everyone")]
+       (page-html/view
+        :body
+        (for [character characters]
+          [:li (:chars/name character) " - " (or (:chars/school character) "No affiliation")]))))}))
 
 ;; partial serves to provide system to handlers
 ;; f(system, request) -> response becomes f(request) -> response
