14ff647df54f9ee19d6a6955f75005fc57c06249
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-12-21 17:23:56 +0100

    Clean: split sql queries and refactor to-html-list

--- src/dashboard/queries/sql.clj
+++ src/dashboard/queries/sql.clj
@@ -3,16 +3,41 @@
             [dashboard.shared-html.core :as page-html]
             [hiccup2.core :as hiccup]
             [honey.sql :as sql]
-            [next.jdbc :as jdbc]))
+            [incanter.charts :as chart]
+            [incanter.core :as graph]
+            [next.jdbc :as jdbc])
+
+  ;; (:import (java.io FileOutputStream
+  ;;                   ByteArrayOutputStream
+  ;;                   ByteArrayInputStream))
+  )
+
+;; (set! *warn-on-reflection* true)
 
 (def characters {:select [:*]
-                 :from   [:story.chars]})
+                 :from   [:story.chars]
+                 :order-by [[:chars.name :asc]]})
+
+(def char-school {:select [:chars.name :chars.school]
+                  :from   [:story.chars]
+                  :order-by [[:chars.name :asc]]})
+
+(def char-age {:select [:chars.name :chars.age]
+               :from   [:story.chars]
+               :order-by [[:chars.name :asc]]})
 
 (defn select
   [db query]
   (jdbc/execute!
    db
    (sql/format query)))
+
+(defn hello
+  []
+  (str
+   (hiccup/html
+    (page-html/view)
+    [:h1 (str "Hello")])))
 
 (defn to-html-list
   [rows]
@@ -24,8 +49,7 @@
     (for [row rows]
       [:li (string/join
             " - "
-            (remove string/blank?
-                    (map row (keys row))))]))))
+            (remove nil? (vals row)))]))))
 
 (defn to-html-table
   [rows]
