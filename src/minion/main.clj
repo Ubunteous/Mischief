6f0063f3817c6cf609736825f59ef2c9f75214ad
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-04-15 01:57:11 +0200

    CLI updated

--- src/minion/main.clj
+++ src/minion/main.clj
@@ -5,14 +5,16 @@
             [minion.parts.subcmd :as subcmd]))
 
 (def table
-  [{:cmds ["create"] :fn subcmd/create-table :args->opts [:table]}
+  [
+   ;; {:cmds ["create"] :fn subcmd/create-table :args->opts [:table]}
    ;; {:cmds ["create" "table"]
    ;;  :fn subcmd/create-table
    ;;  :args->opts [:target]
    ;;  :spec {:target {:validate (and (partial subcmd/file-exists? "csv")
    ;;                                 partial subcmd/file-exists? "edn")}}}
    {:cmds ["select"] :fn subcmd/select :args->opts [:table]}
-   {:cmds ["update"] :fn subcmd/update :args->opts [:table]}
+   {:cmds ["upsert"] :fn subcmd/upsert-csv :args->opts [:table]}
+   {:cmds ["update"] :fn subcmd/upsert-csv :args->opts [:table]}
    {:cmds [] :fn subcmd/help}])
 
 (cli/dispatch table *command-line-args*)
