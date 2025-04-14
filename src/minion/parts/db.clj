129ba08c44f2b2e903099deaaffc24564b1209ee
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-04-15 01:56:53 +0200

    Upsert command added

--- src/minion/parts/db.clj
+++ src/minion/parts/db.clj
@@ -5,35 +5,128 @@
 
 (set! *warn-on-reflection* true)
 
+;;;;;;;;;;;
+;; SETUP ;;
+;;;;;;;;;;;
+
+(def env (into {}
+               (map (fn [pair]
+                      (let [[k v] (clojure.string/split pair #"=")]
+                        [(keyword k) v]))
+                    (clojure.string/split (slurp ".env") #"\n"))))
+
+(def res-path (:RES_PATH env))
+
 (def args
-  (let [env (into {}
-                  (map (fn [pair]
-                         (let [[k v] (clojure.string/split pair #"=")]
-                           [(keyword k) v]))
-                       (clojure.string/split (slurp ".env") #"\n")))]
-    {:dbtype   "postgresql"
-     :host     (:POSTGRES_HOST env)
-     :dbname   (:POSTGRES_DBNAME env)
-     :user     (:POSTGRES_USERNAME env)
-     :password (:POSTGRES_PASSWORD env)
-     :port     (Integer/parseInt (:POSTGRES_PORT env))}))
+  {:dbtype   "postgresql"
+   :host     (:POSTGRES_HOST env)
+   :dbname   (:POSTGRES_DBNAME env)
+   :user     (:POSTGRES_USERNAME env)
+   :password (:POSTGRES_PASSWORD env)
+   :port     (Integer/parseInt (:POSTGRES_PORT env))})
 
 (defn table-exists?
-  [db table]
-  (not (empty?
-        (pg/execute! db (hsql/format
-                         {:select []
-                          :from [:information_schema.tables]
-                          :where [:and
-                                  [:= :table_schema "story"]
-                                  [:= :table_name table]]})))))
+  [table]
+  (boolean (seq
+            (pg/execute! args (hsql/format
+                               {:select []
+                                :from [:information_schema.tables]
+                                :where [:and
+                                        [:= :table_schema "story"]
+                                        [:= :table_name table]]})))))
+
+;;;;;;;;;;;;
+;; SELECT ;;
+;;;;;;;;;;;;
+
+(defn select
+  [query]
+  (pg/execute! args (hsql/format query)))
+
+(defn upsert
+  [table query]
+  (let [cols (into [] (keys (first query)))]
+    (pg/execute! args
+                 (hsql/format
+                  {:insert-into [(symbol table)]
+                   :values query
+                   :on-conflict :name :do-update-set cols
+                   :returning :*}))))
+
+(defn update
+  [table query]
+  (pg/execute! args
+               (hsql/format
+                {:update [(symbol table)]
+                 :set query
+                 :on-conflict :name :do-update-set :name
+				 ;; :on-duplicate-key-update :*
+                 :returning :*})))
 
 (defn select-all
-  [db table]
+  [table]
   (try
-    (pg/execute! db (hsql/format {:select [:*] :from [(symbol table)]}))
+    (pg/execute! args (hsql/format {:select [:*] :from [(symbol table)]}))
     (catch Exception e
       (println "\nCould not find table" table ". Try one of these instead:\n"
-               (map :tables/table_name (pg/execute! db (hsql/format {:select [:table_name]
-                                                                     :from [:information_schema.tables]
-                                                                     :where [:= :table_schema "story"]})))))))
+               (map :tables/table_name
+                    (pg/execute! args (hsql/format {:select [:table_name]
+                                                    :from [:information_schema.tables]
+                                                    :where [:= :table_schema "story"]})))))))
+
+;;;;;;;;;
+;; GET ;;
+;;;;;;;;;
+
+(defn get-foreign-cols
+  [table]
+  (map #(keyword (:key_column_usage/column_name %))
+       (select {:select [:information_schema.key_column_usage.column_name]
+                :from [:information_schema.table_constraints]
+                :join
+                [:information_schema.key_column_usage
+                 [:=
+                  :information_schema.key_column_usage.constraint_name
+                  :information_schema.table_constraints.constraint_name]]
+                :where
+                [:and
+                 [:= :information_schema.table_constraints.constraint_type "FOREIGN KEY"]
+                 [:= :information_schema.table_constraints.table_schema "story"]
+                 [:= :information_schema.table_constraints.table_name table]]})))
+
+(defn get-cols-types
+  [table]
+  (apply merge
+         (map #(hash-map (keyword (:columns/column_name %)) (:columns/data_type %))
+              (select
+               {:select [:column_name :data_type]
+                :from [:information_schema.columns]
+                :where [:= :table_name table]}))))
+
+;;;;;;;;;;;;;;;;;
+;; PRE-PROCESS ;;
+;;;;;;;;;;;;;;;;;
+
+(defn get-cols-converter
+  [col-types]
+  (let [m (dissoc col-types :id)
+        f #(case %
+             "integer" (fn [s] (if (re-matches #"\d+" s) (Integer/parseInt s) s))
+             "boolean" (fn [s] (case s "true" true "false" false))
+             "character varying" (fn [s] s))]
+    (into {} (map (fn [[k v]] [k (f v)]) m))))
+
+(defn convert-columns [func-map value-map]
+  (into {} (map (fn [[k f]]
+                  [k (f (get value-map k))])
+                func-map)))
+
+(defn replace-foreign-keys [hash-maps keys]
+  (map (fn [m]
+         (reduce (fn [acc k]
+                   (if (contains? acc k)
+                     (assoc acc k {:select [:id] :from [k] :where [:= :name (str (k acc))]})
+                     acc))
+                 m
+                 keys))
+       hash-maps))
--- src/minion/parts/subcmd.clj
+++ src/minion/parts/subcmd.clj
@@ -1,15 +1,56 @@
 (ns minion.parts.subcmd
   (:require [babashka.fs :as fs]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.pprint]
+            [clojure.string]
             [minion.parts.db :as db]))
 
-(defn create-table [m]
-  (assoc m :fn :create-table))
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ; header
+            (map keyword) ; drop string keys
+            repeat)
+       (rest csv-data)))
 
-(defn select [target]
+(defn read-csv [csv-filename]
+  (with-open [reader (io/reader (str db/res-path "csv/" csv-filename ".csv"))]
+    (doall
+     (csv-data->maps (csv/read-csv reader)))))
+
+(defn process-csv-keys
+  [csv-map]
+  (let [rename-keys (fn [m f] (into {} (for [[k v] m] [(f k) v])))
+        symbol-lower-case (fn [s] (keyword (clojure.string/lower-case (name s))))]
+    (for [row csv-map]
+      (rename-keys row symbol-lower-case))))
+
+;; (defn create-table [m]
+;;   (assoc m :fn :create-table))
+
+(defn select
+  [target]
   (let [table (get-in target [:opts :table])]
-    (clojure.pprint/pprint (db/select-all db/args table))))
+    (clojure.pprint/pprint (db/select-all table))))
 
-(defn update [m])
+(defn upsert-csv
+  [target]
+  (let [table-name (get-in target [:opts :table])
+        csv-data (process-csv-keys
+                  (read-csv table-name))
+        foreign-cols (db/get-foreign-cols table-name)
+        col-types (db/get-cols-types table-name)
+        col-converter (db/get-cols-converter col-types)
+        cols-typed (map (partial db/convert-columns col-converter) csv-data)
+        processed (db/replace-foreign-keys cols-typed foreign-cols)]
+
+    (println "\nCurrent State:")
+    (clojure.pprint/print-table (db/select-all table-name))
+
+    (db/upsert table-name processed)
+
+    (println "\nTransaction Complete:")
+    (clojure.pprint/print-table (db/select-all table-name))))
 
 (defn help [m]
   ;; also try (cli/dispatch table args)
